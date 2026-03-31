25742af73e4895704b1b1aaab35de6c4ad272ae8
ulkbm2k25 <bamigbolaulrich@gmail.com>
2026-04-01 00:33:32 +0100

    J'enlève le code du mode commentaire

--- compteur_de_mot_specifique.py
+++ compteur_de_mot_specifique.py
@@ -1,4 +1,4 @@
-"""print("===Word encounter====")
+print("===Word encounter====")
 text = input("Enter your sentence/text here :kk\n").lower()
 word = input("Enter the word you want to know how many time repeated : ")
 loword = word.lower()
@@ -8,12 +8,10 @@ for w in words:
     
     if w == loword:
         t += 1
-print(f"'{word}' is repeated {t} times in the text")"""
+print(f"'{word}' is repeated {t} times in the text")
 
 
-""" We can simply use the function count()
-t = text.count(loword)
-"""
+
 
 
 
